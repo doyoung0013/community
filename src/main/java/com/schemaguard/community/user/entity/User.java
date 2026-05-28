c3189797158e116b12ba19859cde4301f038664e
kdy <sksl6134@gmail.com>
2026-05-28 19:09:24 +0900

    test: drop email

--- src/main/java/com/schemaguard/community/user/entity/User.java
+++ src/main/java/com/schemaguard/community/user/entity/User.java
@@ -18,8 +18,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 100)
-    private String email;
 
     @Column(nullable = false, length = 255)
     private String password;
